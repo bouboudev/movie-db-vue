05939c62ebe454440cdcf60b470aeb1b1b2ef2c0
Bouzid Krita <bouzid.krita@hotmail.fr>
2021-11-02 19:05:52 +0100

    first commit

---
+++ src/views/MovieDetail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="movie-detail">
+      <!-- Nom du film dynamique en fonction du lien de l'utilisateur -->
+      Detail {{$route.params.id}}
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
